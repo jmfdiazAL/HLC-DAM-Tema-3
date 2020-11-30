cbcbd5ec11c83eba915260aa8b7325202ebd53f2
José Manuel Fernández Díaz <josemanuel.fernandez@iesalandalus.org>
2020-11-30 10:31:01 +0100

    30/11/2020
    Actualización del JDK

--- Kotlin/HLC03_EjemplosCodigo01_07/02_Ud3_EntenderHilos1/app/src/main/java/com/pms/a02_ud3_entenderhilos1/MainActivity.kt
+++ Kotlin/HLC03_EjemplosCodigo01_07/02_Ud3_EntenderHilos1/app/src/main/java/com/pms/a02_ud3_entenderhilos1/MainActivity.kt
@@ -3,6 +3,7 @@ package com.pms.a02_ud3_entenderhilos1
 import android.app.Activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
 import android.widget.TextView
 
@@ -20,17 +21,17 @@ class MainActivity : AppCompatActivity() {
     fun startCounter(view: View?) {
         for (i in 0..1000) {
             txtView!!.text = i.toString()
-
-            /*      // pausa para dar tiempo a que se muestre el valor antes de pasar al
+            /*
+            // pausa para dar tiempo a que se muestre el valor antes de pasar al
             // siguiente (esta estrategia provoca un fallo, se necesita un hilo secundario)
             try {
                 Thread.sleep(1000);
-            } catch (InterruptedException e) {
+            } catch (e: InterruptedException) {
                 Log.d("Threading", e.getLocalizedMessage());
             }
-*/
             // si no se intentara hacer esta pausa, el programa terminaría bien
             // pero sólo mostraría el 1000
+            */
         }
     }
 
