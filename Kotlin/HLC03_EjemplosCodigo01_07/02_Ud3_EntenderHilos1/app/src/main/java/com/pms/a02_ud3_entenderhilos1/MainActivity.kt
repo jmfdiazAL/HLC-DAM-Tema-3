00e5ed44b2f694e6da250175ed051db11f7ba015
José Manuel Fernández Díaz <josemanuel.fernandez@iesalandalus.org>
2020-12-03 09:58:37 +0100

    Update MainActivity.kt

--- Kotlin/HLC03_EjemplosCodigo01_07/02_Ud3_EntenderHilos1/app/src/main/java/com/pms/a02_ud3_entenderhilos1/MainActivity.kt
+++ Kotlin/HLC03_EjemplosCodigo01_07/02_Ud3_EntenderHilos1/app/src/main/java/com/pms/a02_ud3_entenderhilos1/MainActivity.kt
@@ -21,14 +21,27 @@ class MainActivity : AppCompatActivity() {
     fun startCounter(view: View?) {
         for (i in 0..1000) {
             txtView!!.text = i.toString()
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
             /*
             // pausa para dar tiempo a que se muestre el valor antes de pasar al
             // siguiente (esta estrategia provoca un fallo, se necesita un hilo secundario)
             try {
+<<<<<<< Updated upstream
                 Thread.sleep(1000);
             } catch (e: InterruptedException) {
                 Log.d("Threading", e.getLocalizedMessage());
             }
+=======
+                Thread.sleep(1000)
+            } catch (e: InterruptedException) {
+                Log.d("Threading", e.localizedMessage)
+            }
+            */
+
+>>>>>>> Stashed changes
             // si no se intentara hacer esta pausa, el programa terminaría bien
             // pero sólo mostraría el 1000
             */
